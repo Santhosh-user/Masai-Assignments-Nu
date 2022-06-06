194a926063b3a27ac17c152251fdaed952935300
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-06-06 21:29:00 +0530

    1

--- Sort,filter,pagination/Backend/src/index.js
+++ Sort,filter,pagination/Backend/src/index.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
-const connect = require("./configs/db")
+
 
 const app = express
+
+const connect = () =>{
+    return mongoose.connect("mongodb+srv://Nbackend1:Nbackend1@cluster0.w5hvn.mongodb.net/?retryWrites=true&w=majority")
+}
+
+
+app.length("/", (req,res)=>{
+    res.send(movies)
+})
 
 
 
@@ -16,4 +25,4 @@ app.listen(2345, async()=>{
         console.error(err.message)
     }
     console.log("listening on port 2345")
-})
+}) 
