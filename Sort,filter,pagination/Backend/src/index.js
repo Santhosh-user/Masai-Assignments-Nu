f669fd5c9328b9e82a33b1d31a029adc09ec6f97
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-06-06 22:16:28 +0530

    1

--- Sort,filter,pagination/Backend/src/index.js
+++ Sort,filter,pagination/Backend/src/index.js
@@ -1,45 +1,39 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
-
-
 const app = express()
 
 const connect = () =>{
     return mongoose.connect("mongodb+srv://Nbackend1:Nbackend1@cluster0.w5hvn.mongodb.net/?retryWrites=true&w=majority")
 }
 
-
-
 //schema
 
 const movieSchema = new mongoose.Schema({
-    id: {type: Number},
+    id: {type: String},
     movie_name: {type: String},
     critic_rating: {type: Number},
     year: {type: Number},
     viewer_rating: {type: Number},
 })
 
-
 //model
 
 const Movie = mongoose.model("movie", movieSchema)
 
-
-app.get("/", (req,res)=>{
-    const movies = User.find().lean().exec()
+app.get("/movies", async (req,res)=>{
+    const movies = await Movie.find().lean().exec()
     res.send(movies)
 })
 
 
-
-app.listen(2345, async()=>{
+app.listen(2345, async ()=>{
     try{
         await connect()
+        console.log("listening on port 2345")
     }
     catch (err){
         console.error(err.message)
     }
-    console.log("listening on port 2345")
+    
 }) 
