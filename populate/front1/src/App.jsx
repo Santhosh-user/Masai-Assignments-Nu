38bc659878eef5c316b0ad5e445e82e25aa9b171
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-06-08 09:12:23 +0530

    1

--- populate/front1/src/App.jsx
+++ populate/front1/src/App.jsx
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import AllUser from './components/allUser';
+import  NewUser  from './components/newuser';
 
 function App() {
   return (
     <div className="App">
       <div>
-        <AllUser></AllUser>
+       <NewUser></NewUser>
       </div>
       
     </div>
   );
 }
+
 
 export default App;
--- populate/front1/src/components/newuser.jsx
+++ populate/front1/src/components/newuser.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
+import axios from "axios"
 
-export const NewUser = () =>{
+const NewUser = () =>{
     const [gen, setGen] = useState({
         first_name: "",
        
     })
     const [addre, setAddre]= useState({
         address1: "",
-        address2: ""
+        address2: "",
     })
 
     const createPerson = (e)=>{
@@ -23,8 +24,10 @@ export const NewUser = () =>{
         })
     }
 
+    const processData=(e)=>{
+        e.preventDefault()
 
-    axios.post("http://localhost:2345/users", {
+        axios.post("http://localhost:2345/users", {
         first_name: gen.first_name,
         address: [ {address1: addre.address1,
                 address2: addre.address2}
@@ -37,22 +40,28 @@ export const NewUser = () =>{
         console.log(error);
       });
 
+    }
+    
+
       return (
           <div>
               <form action="">
                   <label htmlFor="">First Name</label>
-                  <input onChange={createPerson} id={first_name} type="text" />
+                  <input onChange={createPerson} id={"first_name"} type="text" />
               </form>
               <br />
               <br />
               <form action="">
                   <label htmlFor="">address1</label>
-                  <input onChange={createAddre} id={address1} type="text" />
+                  <input onChange={createAddre} id={"address1"} type="text" />
                   <label htmlFor="">address2</label>
-                  <input onChange={createAddre} id={address2} type="text" />
+                  <input onChange={createAddre} id={"address2"} type="text" />
+                  <button onClick={processData}>Create New USer</button>
               </form>
           </div>
       )
 
 
 }
+
+export default NewUser
